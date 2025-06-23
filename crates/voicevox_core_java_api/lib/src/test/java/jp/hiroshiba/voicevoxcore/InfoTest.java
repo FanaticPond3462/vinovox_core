62ba55c09dab0ec38bf5732f6c87f2dc257972a1
FanaticPond3462 <111404437+FanaticPond3462@users.noreply.github.com>
2025-06-23 17:31:18 +0900

    Chore: 修正

--- crates/voicevox_core_java_api/lib/src/test/java/jp/hiroshiba/voicevoxcore/InfoTest.java
+++ crates/voicevox_core_java_api/lib/src/test/java/jp/hiroshiba/voicevoxcore/InfoTest.java
@@ -4,7 +4,6 @@
 package jp.hiroshiba.voicevoxcore;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
